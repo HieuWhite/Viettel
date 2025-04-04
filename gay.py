556c376d42b7a8c1ac21c7f1fee7ff45b96b77f7
HieuWhite <89020784+HieuWhite@users.noreply.github.com>
2025-04-04 11:14:06 +0700

    Update gay.py

--- gay.py
+++ gay.py
@@ -32,11 +32,26 @@ VIETTEL.COM.VN
 djg nkdjw  jke 
 e jw gkwkjwe jwejwejwre
  ewtjwtejewjngjdndnfioniow 
-VIETTEL.COM.VN
-djg nkdjw  jke 
-e jw gkwkjwe jwejwejwre
- ewtjwtejewjngjdndnfioniow 
-VIETTEL.COM.VN
+A newly discovered phishing-as-a-service (PhaaS) operation that researchers call Morphing Meerkat, has been using the DNS over HTTPS (DoH) protocol to evade detection.
+
+The platform also leverages DNS email exchange (MX) records to identify victims’ email providers and to dynamically serve spoofed login pages for more than 114 brands.
+
+Morphing Meerkat has been active since at least 2020 and it was discovered by security researchers at Infoblox. Although the activity has been partially documented, it went mostly under the radar for years.
+
+Large-scale phishing operation
+Morphing Meerkat is a PhaaS platform providing a complete toolkit for launching effective, scalable, and evasive phishing attacks that require minimal technical knowledge.
+
+It features a centralized SMTP infrastructure to distribute spam emails, with 50% of the traced emails originating from internet services provided by iomart (UK) and HostPapa (US).
+
+The operation can impersonate more than 114 email and service providers, including Gmail, Outlook, Yahoo, DHL, Maersk, and RakBank, delivering messages with subject lines crafted to prompt urgent action like “Action Required: Account Deactivation.”
+
+The emails are delivered multiple languages, including English, Spanish, Russian, and even Chinese, and can spoof sender names and addresses.
+
+If the victim clicks on the malicious link in the message, they go through a chain of open redirect exploits on ad tech platforms like Google DoubleClick, frequently involving compromised WordPress sites, fake domains, and free hosting services.
+
+Once the victim reaches the final destination, the phishing kit loads and queries the victim’s email domain’s MX record using DoH via Google or Cloudflare.
+
+Based on the result, the kit loads a fake login page with the victim's email address filled automatically.
 djg nkdjw  jke 
 e jw gkwkjwe jwejwejwre
  ewtjwtejewjngjdndnfioniow 
